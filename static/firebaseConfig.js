df8160bc966882ef77b76b50e48cd0179fc5aff8
Sam Girshovich <sam.girshovich@gmail.com>
2021-01-22 10:55:23 -0500

    firebase analytics

---
+++ static/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCnbwUW_E6AM-jOUocn393xTQG0rKZXJ58",
+    authDomain: "weatherly-302219.firebaseapp.com",
+    projectId: "weatherly-302219",
+    storageBucket: "weatherly-302219.appspot.com",
+    messagingSenderId: "2122407348",
+    appId: "1:2122407348:web:bdfca43470c4816e98a6e1",
+    measurementId: "G-JFRKGHNZXJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
